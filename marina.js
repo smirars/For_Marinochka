b5bc91bf618625a76a791077b5ef43de5b2035c5
Ignatiy Glushkov <ignat.glushkov@mail.ru>
2024-06-23 02:33:47 +0300

    refactoring starts

--- marina.js
+++ marina.js
@@ -1,32 +1,60 @@
-const video = document.getElementById("video");
+// const video = document.getElementById("video");
 
-function changeVideo() {
-    video.src = '2.mp4';
-    goToBottom();
-  };
+// function changeVideo() {
+//     video.src = '2.mp4';
+//     goToBottom();
+//   };
 
-const button = document.querySelector('.first');
+// const button = document.querySelector('.first');
 
-function showButtonAndStopVideo() {
-    document.getElementById("video").src = '3.mp4';
-    const button1 = document.querySelector('.second'); 
-    const video = document.getElementById('video');
-    button1.addEventListener('click', () => {
-      setTimeout(() => {
-        video.pause();
-        button.style.visibility = 'visible'; 
-      }, 3000);
-    });
-  }
+// function showButtonAndStopVideo() {
+//     document.getElementById("video").src = '3.mp4';
+//     const button1 = document.querySelector('.second'); 
+//     const video = document.getElementById('video');
+//     button1.addEventListener('click', () => {
+//       setTimeout(() => {
+//         video.pause();
+//         button.style.visibility = 'visible'; 
+//       }, 3000);
+//     });
+//   }
 
-const stopBtn =  document.querySelector('.pauseBtn');
-const playBtn = document.querySelector('.playBtn');
+// const stopBtn =  document.querySelector('.pauseBtn');
+// const playBtn = document.querySelector('.playBtn');
 
-playBtn.addEventListener('click', function () {
-  video.play();
-});
+// playBtn.addEventListener('click', function () {
+//   video.play();
+// });
 
-stopBtn.addEventListener('click',function(){
-  video.pause();
-});
+// stopBtn.addEventListener('click',function(){
+//   video.pause();
+// });
 
+const configName = "config.json";
+let config = null;
+
+const init = () => {
+  console.log("INIT");
+
+
+  fetch(configName)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Теперь data является объектом JavaScript
+                
+                console.log(data);
+                config = data;
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+
+
+}
+
+init();
