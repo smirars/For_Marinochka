cf0fcab016cd57bb7e3ae783afa98e9d2eb365a7
Ignatiy Glushkov <ignat.glushkov@mail.ru>
2024-06-23 17:06:40 +0300

    almost done

--- marina.js
+++ marina.js
@@ -1,7 +1,67 @@
 
-const configName = "config.json";
-const player = videojs('videoPlayer');
-let config = null;
+const stepController = {
+    configName: "config.json",
+    config: null,
+
+    getStep: function(step_id) {
+        if (!step_id )
+          console.error("nextStep: invalid step_id", step_id)
+    
+        let step = this.config[step_id]
+    
+        if (!step)
+            console.error("nextStep: no such step under step_id", step_id)
+    
+        return step
+    },
+
+    nextStep: function(step_id, is_initial_step = false) {
+        let step = this.getStep(step_id)
+    
+        if(step.choices.length > 0) {
+    
+            if (step.next_step)
+                console.warn("WARNING: choices and next_step are both not empty! step_id:", step_id)
+    
+            videoController.setAutoNextStep("")
+            overlayController.clean()
+            overlayController.setTitle(step.title)
+    
+            step.choices.forEach(choice => {
+                overlayController.addButton(choice.next_step, choice.text, choice.pic_src)
+            });
+    
+        } else
+            videoController.setAutoNextStep(step.next_step)
+    
+
+    
+        videoController.setVideoSrc(step.src)
+    
+        // Первое видео не запустится автоматически
+        if (!is_initial_step)
+            videoController.playVideo() 
+    },
+
+    init: function() {
+  
+        fetch(this.configName)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.config = data;
+            videoPlayer.appendChild(overlayController.overlay);
+            this.nextStep(this.config.initial_step, true)
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    }
+}
 
 
 const overlayController = {
@@ -9,6 +69,8 @@ const overlayController = {
     overlay: document.getElementById('overlay'),
     overlayTitle: document.getElementById('overlayTitle'),
     buttonContainer: document.getElementById('buttonContainer'),
+
+    visible: false,
 
     setTitle: function(title) {
         this.overlayTitle.textContent = title;
@@ -21,111 +83,78 @@ const overlayController = {
         img.alt = text;
         button.appendChild(img);
         button.onclick = () => {
-            console.log("button clicked")
-            nextStep(step_id)
+            console.info("next step clicked:", step_id)
+            stepController.nextStep(step_id)
         };
         this.buttonContainer.appendChild(button);
     },
 
     clean: function() {
+        overlayController.hide()
         this.overlayTitle.textContent = '';
         this.buttonContainer.innerHTML = '';
+    },
+
+    hide: function () {
+        if (!this.visible)
+            return
+        this.overlay.classList.remove('active')
+        this.overlay.classList.add('fade-out')
+        this.overlay.classList.remove('fade-in')
+        this.visible = false
+    },
+
+    show: function () {
+        if (this.visible)
+            return
+        this.overlay.classList.add('active')
+        this.overlay.classList.remove('fade-out')
+        this.overlay.classList.add('fade-in')
+        this.visible = true
     }
 
 }
 
 
-init();
+const videoController = {
+    player: videojs('videoPlayer'),
 
-
-function init() {
-  
-    fetch(configName)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        config = data;
-        
-        videoPlayer.appendChild(overlayController.overlay);
-        
-
-        player.on('timeupdate', () => {
-            if (player.duration() - player.currentTime() < 0.5) {
-                overlayController.overlay.style.opacity = 1.0;
-            } else {
-                overlayController.overlay.style.opacity = 0.0;
-            }
-        });
-
-
-        nextStep(config.initial_step)
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
+    setAutoNextStep: function(step_id) {
+        if (step_id) {
           
-}
+            this.player.on('ended', () => { stepController.nextStep(step_id) });
+            this.player.on('timeupdate', () => {});
 
-function getStep(step_id) {
-    if (!step_id) {
-      console.error("nextStep: invalid step_id", step_id)
-    }
+        } else {
+          
+            this.player.on('ended', () => {});
+            this.player.on('timeupdate', () => {
+                this.player.remainingTime() < 0.5  ? overlayController.show() 
+                                            : overlayController.hide()
+            });
 
-    return config[step_id]
-}
+        }
+    }, 
 
-function setVideoSrc(src) {
-    player.src({ type: 'video/mp4', src: src });
-}
-
-function playVideo() {
-    player.ready(function() {
-        console.log('The player is ready');
-        // player.muted(true);
-        player.play().then(() => {
-           console.log('Video is playing');
-        }).catch(error => {
-            console.error('Failed to play video:', error);
+    setVideoSrc: function(src) {
+        this.player.src({ type: 'video/mp4', src: src });
+    },
+    
+    playVideo: function() {
+        this.player.load();
+        this.player.ready(function() {
+            // player.muted(true);
+            videoController.player.play().then(() => {
+            //    console.log('Video is playing');
+            }).catch(error => {
+                // тут возникает ошибка при автоматическом запуске видео
+                // console.error('Failed to play video:', error);
+            });
         });
-    });
+    }
 }
 
-
-
-function setAutoNextStep(step_id) {
-  if (step_id)
-    player.on('ended', function() { nextStep(step_id) });
-  else
-    player.on('ended', function() {});
-}
-
-
-function cleanOverlay() {
-
-}
-
-
-function nextStep(step_id) {
-  let step = getStep(step_id)
-  setVideoSrc(step.src)
-  
-  if(step.choices) {
-    setAutoNextStep("")
-    overlayController.clean()
-    overlayController.setTitle(step.title)
-    step.choices.forEach(choice => {
-        overlayController.addButton(choice.next_step, choice.text, choice.pic_src)
-    });
-
-  } else
-    setAutoNextStep(step.next_step)
-
-//   playVideo() //Warning первое видео не запустится!
-}
+stepController.init();
 
 
 
